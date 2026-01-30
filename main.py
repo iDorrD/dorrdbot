8a8af25aa331809e0633c4fa293e87e6c3a74a1e
iDorrD <dorrd2002@gmail.com>
2026-01-30 16:56:31 +0100

    Actualizar el estado del bot para mostrar "a DorrD"

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ async def on_ready():
     await bot.change_presence(
         activity=discord.Activity(
             type=discord.ActivityType.watching,
-            name="a los nuevos miembros 👀"
+            name="a DorrD"
         )
     )
 
