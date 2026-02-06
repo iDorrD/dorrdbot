8dce31dd0e165d8653fa903b2bf0aebb2813afd3
iDorrD <dorrd2002@gmail.com>
2026-02-06 12:36:22 +0100

    Actualizar el estado de actividad del bot a "dorrdclubapp.vercel.app/discord"

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def on_ready():
     await bot.change_presence(
         activity=discord.Activity(
             type=discord.ActivityType.watching,
-            name="Moderando DorrD Club 💜"
+            name="dorrdclubapp.vercel.app/discord"
         )
     )
 
